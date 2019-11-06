9182cd751f51f5ee815ad08b9f38af1915dc23c3
Caroline Monthoux <caroline.monthoux@heig-vd.ch>
2019-11-06 16:09:56 +0100

    implements CharacterManager tests

--- src/test/java/ch/heigvd/amt/projectOne/services/dao/CharacterManagerTest.java
+++ src/test/java/ch/heigvd/amt/projectOne/services/dao/CharacterManagerTest.java
@@ -1,19 +1,16 @@
 package ch.heigvd.amt.projectOne.services.dao;
 
+import ch.heigvd.amt.projectOne.model.Character;
 import org.arquillian.container.chameleon.deployment.api.DeploymentParameters;
 import org.arquillian.container.chameleon.deployment.maven.MavenBuild;
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
 import org.jboss.arquillian.transaction.api.annotation.Transactional;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import javax.ejb.DuplicateKeyException;
 import javax.ejb.EJB;
-import java.sql.SQLException;
+
+import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
 @MavenBuild
@@ -26,7 +23,117 @@ public class CharacterManagerTest {
 
     @Test
     @Transactional(TransactionMode.ROLLBACK)
-    public void itShouldBePossibleToCreateAUser() throws DuplicateKeyException, SQLException {
-        characterManager.addCharacter("Cake", "test", false);
+    public void itShouldBePossibleToCreateCharacter() {
+        String name = "Cake";
+        String password = "test";
+        boolean isAdmin = false;
+
+        characterManager.addCharacter(name, password, isAdmin);
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToGetACharacterByItsName() {
+        String name = "Cake";
+        String password = "test";
+        boolean isAdmin = false;
+
+        /* Character creation */
+        if (characterManager.addCharacter(name, password, isAdmin)) {
+            Character cake = characterManager.getCharacterByUsername(name);
+
+            assertNotNull(cake);
+        }
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToGetACharacterByItsId() {
+        String name = "Cake";
+        String password = "test";
+        boolean isAdmin = false;
+
+        /* Character creation */
+        if (characterManager.addCharacter(name, password, isAdmin)) {
+            Character cakeByName = characterManager.getCharacterByUsername(name);
+            int id = cakeByName.getId();
+            Character cakeById = characterManager.getCharacterById(id);
+
+            assertEquals(cakeByName, cakeById);
+        }
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToUpdateACharacter() {
+        String name = "Cake";
+        String password = "test";
+        boolean isAdmin = false;
+
+        /* Character creation */
+        if (characterManager.addCharacter(name, password, isAdmin)) {
+            Character cake = characterManager.getCharacterByUsername(name);
+            String newName = "Lie";
+            String newPassword = "bye";
+
+            /* Character update */
+            if (characterManager.updateCharacter(cake.getId(), newName, newPassword, isAdmin, true)) {
+                Character newCake = characterManager.getCharacterByUsername("Lie");
+
+                assertEquals(newCake.getName(), newName);
+                assertNotEquals(newCake.getName(), name);
+            }
+        }
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToCheckIfAPasswordIsCorrect() {
+        String name = "Cake";
+        String password = "test";
+        boolean isAdmin = false;
+
+        /* Character creation */
+        if (characterManager.addCharacter(name, password, isAdmin)) {
+            Character cake = characterManager.getCharacterByUsername("Cake");
+            boolean checkSuccess = characterManager.checkPassword(cake.getName(), password);
+
+            assertTrue(checkSuccess);
+        }
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToCheckIfAUsernameIsFree() {
+        String name = "Cake";
+        String password = "test";
+        boolean isAdmin = false;
+
+        /* Character creation */
+        if (characterManager.addCharacter(name, password, isAdmin)) {
+            boolean freeUsername = characterManager.isUsernameFree(name);
+
+            assertFalse(freeUsername);
+        }
+    }
+
+    @Test
+    @Transactional(TransactionMode.ROLLBACK)
+    public void itShouldBePossibleToDeleteACharacter() {
+        String name = "Cake";
+        String password = "test";
+        boolean isAdmin = false;
+
+        /* Character creation */
+        if (characterManager.addCharacter(name, password, isAdmin)) {
+            Character cake = characterManager.getCharacterByUsername(name);
+
+            /* Character deletion */
+            boolean deletedCharacter = characterManager.deleteCharacter(cake.getId());
+            cake = characterManager.getCharacterById(cake.getId());
+
+            assertNull(cake);
+            assertTrue(deletedCharacter);
+        }
     }
 }
