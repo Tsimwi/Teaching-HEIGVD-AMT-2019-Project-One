b925678213c461549e880b91e8729bc12d537300
Caroline Monthoux <caroline.monthoux@heig-vd.ch>
2019-11-11 22:28:55 +0100

    adds comment on daos

--- src/main/java/ch/heigvd/amt/projectOne/services/dao/CharacterManagerLocal.java
+++ src/main/java/ch/heigvd/amt/projectOne/services/dao/CharacterManagerLocal.java
@@ -9,25 +9,58 @@ import java.util.List;
 @Local
 public interface CharacterManagerLocal {
 
+    /**
+     * Count the number of rows in a table, following a pattern.
+     */
     int countRows(String table, String pattern);
 
+    /**
+     * Return a list of character (USED ONLY TO TEST THE PAGINATION).
+     */
     List<Character> getCharactersForPaginationTest();
 
+    /**
+     * Return a list of characters selected by a specific pattern.
+     */
     List<Character> getCharactersByPattern(String letter, int pageNumber);
 
+    /**
+     * Return a list of characters selected in relation with the page number they are in.
+     */
     List<Character> getCharactersByPage(int pageNumber);
 
+    /**
+     * Add a character in the database.
+     */
     boolean addCharacter(String username, String password, boolean isAdmin);
 
+    /**
+     * Update a character in the database.
+     */
     boolean updateCharacter(int id, String username, String password, boolean isAdmin, boolean updatePassword);
 
+    /**
+     * Return the character with the given ID.
+     */
     Character getCharacterById(int id);
 
+    /**
+     * Return the character with the given username.
+     */
     Character getCharacterByUsername(String username);
 
+    /**
+     * Check whether the given username is free or not in the database.
+     */
     boolean isUsernameFree(String username) throws SQLException;
 
+    /**
+     * Check if the given password matches the one in the database.
+     */
     boolean checkPassword(String username, String password);
 
+    /**
+     * Delete the given character from the database.
+     */
     boolean deleteCharacter(int id);
 }
--- src/main/java/ch/heigvd/amt/projectOne/services/dao/GuildManagerLocal.java
+++ src/main/java/ch/heigvd/amt/projectOne/services/dao/GuildManagerLocal.java
@@ -10,20 +10,44 @@ import java.util.List;
 @Local
 public interface GuildManagerLocal {
 
+    /**
+     * Return the number of guilds in the database.
+     */
     int getNumberOfGuild();
 
+    /**
+     * Return a list of all guilds.
+     */
     List<Guild> getAllGuilds();
 
+    /**
+     * Return the guild with the given ID.
+     */
     Guild getGuildById(int id);
 
+    /**
+     * Return the guild with the given name.
+     */
     Guild getGuildByName(String name);
 
+    /**
+     * Add a guild in the database.
+     */
     boolean addGuild(Guild guild);
 
+    /**
+     * Update a guild in the database.
+     */
     boolean updateGuild(Guild guild);
 
+    /**
+     * Delete a guild in the database.
+     */
     boolean deleteGuild(Guild guild);
 
+    /**
+     * Check whether the given guild name is free in the database.
+     */
     boolean isNameFree(String name);
 
 }
